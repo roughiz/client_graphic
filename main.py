3fda1da307c5b34c5d918d2950b379fcaa7ed65b
Zack <roughi.zakaria@gmail.com>
2017-11-30 22:10:54 +0100

    ad

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ interface=Builder.load_string('''
         GridLayout:
             canvas.before:
                 Color:
-                    rgba: 61, 238, 248, 1 #jaune
+                    rgba: 0.61, 0.238, 0.248, 1 #jaune
                 Rectangle:
                     pos: self.pos
                     size: self.size
